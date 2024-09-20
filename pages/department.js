76bb0b120e619dfb45d41c891b1667d08e00db2b
baqnngg <il8696546@gmail.com>
2024-09-20 11:14:04 +0900

    1

--- pages/department.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react"
-
-export default function department() {
-    useEffect(() => {
-        window.location.href = '/Department-site/test.html';
-    })
-    return null;
-}
-  
